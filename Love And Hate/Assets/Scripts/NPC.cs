4d1bfd0c7abd84df631700976703b88e9f406181
Brighton van den End <brighton@iconica.nl>
2024-08-29 15:24:53 +0200

    NPC pathfinding

--- Love And Hate/Assets/Scripts/NPC.cs
+++ Love And Hate/Assets/Scripts/NPC.cs
@@ -1,18 +1,22 @@
+using System;
 using System.Collections;
 using Player;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.Events;
 using Random = UnityEngine.Random;
+using Rigidbody2D = UnityEngine.Rigidbody2D;
 
 
 public class NPC : MonoBehaviour
 {
     private Transform _target;
-    private Rigidbody2D _rb;
     private float _randSpeed;
     private bool _attacking = false;
+    private Rigidbody2D _rb;
 
     [SerializeField] private float attackSpeed = 2f;
+    [SerializeField] private float attackForce = 25f;
     [SerializeField] private float distanceFromTarget = 2f;
     [SerializeField] private float speed = 2f;
     [SerializeField] private float offset = 2f;
@@ -22,6 +26,7 @@ public class NPC : MonoBehaviour
 
     public UnityEvent<Side> onSideChange;
     public UnityEvent onAttack;
+    private NavMeshAgent _agent;
 
     public void SetSide(Side newSide)
     {
@@ -47,14 +52,25 @@ public class NPC : MonoBehaviour
 
     private void Awake()
     {
-        _rb = GetComponent<Rigidbody2D>();
         _randSpeed = Random.Range(0f, 1f);
+        _agent = GetComponent<NavMeshAgent>();
+        _agent.updateRotation = false;
+        _agent.updateUpAxis = false;
+        _rb = GetComponent<Rigidbody2D>();
     }
 
 
     private void Update()
     {
         if (!_target) return;
+
+        _agent.SetDestination(_target.position);
+
+        if (!_attacking)
+        {
+            var dist = Mathf.Clamp01(Vector3.Distance(transform.position, _target.position) - distanceFromTarget);
+            _agent.speed = ((speed + offset * spread.Evaluate(_randSpeed)) * dist);
+        }
         var distance = Vector3.Distance(_target.position, transform.position);
         if (distance <= distanceFromTarget && !_attacking)
         {
@@ -71,27 +87,22 @@ public class NPC : MonoBehaviour
         onAttack?.Invoke();
         var player = _target.GetComponent<Controller>();
         player.Attacked();
+
         yield return new WaitForSeconds(attackSpeed);
         _attacking = false;
     }
 
-    private void FixedUpdate()
-    {
-        if (!_target)
-        {
-            _rb.velocity = Vector2.zero;
-            return;
-        }
-
-        if (_attacking) return;
-        var dist = Mathf.Clamp(Vector3.Distance(transform.position, _target.position) - distanceFromTarget, -1f, 1f);
-        _rb.velocity = transform.up * ((speed + offset * spread.Evaluate(_randSpeed)) * dist);
-    }
 
     private void OnCollisionEnter2D(Collision2D other)
     {
         var bullet = other.gameObject.GetComponent<Bullet>();
         if (!bullet) return;
         SetSide(bullet.side);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(transform.position, transform.up);
     }
 }
--- Love And Hate/Assets/Scripts/Spawner/SpawnController.cs
+++ Love And Hate/Assets/Scripts/Spawner/SpawnController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Random = UnityEngine.Random;
@@ -9,6 +10,7 @@ namespace Spawner
     {
         [SerializeField] private List<Transform> points = new();
         [SerializeField] private int maxCount;
+        [SerializeField] private float spawnDelay = 1f;
         [SerializeField] private bool randomSpawn;
         [SerializeField] private GameObject prefab;
 
@@ -18,9 +20,14 @@ namespace Spawner
 
         private void Awake()
         {
+            StartCoroutine(Spawn());
+        }
+
+        private IEnumerator Spawn()
+        {
             for (int i = 0; i < maxCount; i++)
             {
-                var obj = GameObject.Instantiate(prefab, transform);
+                var obj = Instantiate(prefab, transform);
                 var randIndex = 0;
                 if (randomSpawn)
                 {
@@ -32,6 +39,7 @@ namespace Spawner
                 }
 
                 obj.transform.position = points[randIndex].transform.position;
+                yield return new WaitForSeconds(spawnDelay);
             }
         }
     }
